b9871104a5b29e1bda7c94d5d2f3ca8e61cbfa63
hutkit <rakihthehole.q.7.p.adlibtum@gmail.com>
2020-01-10 20:47:25 +0900

    １．Numpy導入　２．結果のファイル出力化
    １．APIからの結果をnp.arrayに格納するように変更
    ２．結果は標準出力からファイル出力へ変更

--- scraping_img.py
+++ scraping_img.py
@@ -1,13 +1,23 @@
 # -*- encoding: utf-8 -*-
 
-import json
-from datetime import datetime
+#import json
+#from datetime import datetime
+import numpy as np
 
 ##############################################
-## getImageUrls
-## max_imagesの数だけ、search_qで見つけたURLを取得してreturnする
-## 返り値：
-## image_results["画像のサムネイルURL"]["画像のフルURL"]
+# writeResults
+# リストで渡された結果を特定のファイルに書き込む
+##############################################
+def writeResults(results, file="imgs/default_results.txt"):
+    with open(file, mode='w') as f:
+       f.write("\n".join(results))
+
+##############################################
+# getImageUrls
+# max_imagesの数だけ、search_qで見つけたURLを取得してreturnする
+# 返り値：
+# image_results["画像のサムネイルURL"]["画像のフルURL"]
+# np.arrayで帰ってくる。
 ##############################################
 def getImageUrls(search_q="スーツ", max_images=50):
     # Azure のAPIを登録。
@@ -27,31 +37,37 @@ def getImageUrls(search_q="スーツ", max_images=50):
     client = ImageSearchAPI(CognitiveServicesCredentials(subscription_key))
 
     #max_imagesの数だけ画像を取得。
+    #img_countごとに取得するが、端数は切り捨て。
     for i in range(0,max_images // img_count):
         tmp_results = client.images.search(
             query=search_term,
             count=img_count,
             offset=(i*img_count))
 
-    for tmp_img in tmp_results.Value:
-        image_results.append = [
+    #return用のオブジェクトを作成（今後回収の余地アリ？）
+    image_results = [["thumnail_url", "content_url"]]
+    for tmp_img in tmp_results.value:
+        image_results.append([
             tmp_img.thumbnail_url,
-            tmp_img.content_url]
+            tmp_img.content_url])
 
+    return np.array(image_results)
 
-    return image_results
-
-#時間データのdump化
+#時間データのdump化 ボツ？
+'''
 def date_encoder(obj):
     if isinstance(obj, datetime):
         return obj.isoformat()
+'''
 
 ##############################################
 ## main
 ##############################################
 image_results = getImageUrls("スーツ")
-json_results = open('result.json', 'w')
-json.dump(image_results, json_results, indent=2, default=date_encoder)
+#writeResults(image_results[:, 0], "imgs/results_thumbnails.txt")
+writeResults(image_results[:, 1], "imgs/results_contents.txt")
+'''
+デフォルトの表示部分
 if image_results.value:
     first_image_result = image_results.value[0]
     print("Total number of images returned: {}".format(len(image_results.value)))
@@ -61,3 +77,4 @@ if image_results.value:
     #print("total_Hits: {}".format(image_results.totalEstimatedMatches))
 else:
     print("No image results returned!")
+'''
