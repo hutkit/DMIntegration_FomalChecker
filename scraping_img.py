96882a681c914ffa50374a43086cb2d58f2316f0
hutkit <rakihthehole.q.7.p.adlibtum@gmail.com>
2020-01-24 08:08:56 +0900

    スクレイピング完成

--- scraping_img.py
+++ scraping_img.py
@@ -4,6 +4,10 @@ import numpy as np
 import requests
 import os
 import urllib
+from time import sleep
+import hashlib
+# import sha3
+
 # import http.client
 
 
@@ -40,21 +44,20 @@ def getImageUrls(search_q="スーツ", get_once_images=10, max_images=30):
 
     # max_imagesの数だけ画像を取得。
     # img_countごとに取得するが、端数は切り捨て。
+    image_results = []
+    file_num = 0
     for i in range(0, max_images // img_count):
         tmp_results = client.images.search(
             query=search_term,
             count=img_count,
             offset=(i*img_count))
-
-    # return用のオブジェクトを作成
-    image_results = []
-    file_num = 0
-    for tmp_img in tmp_results.value:
-        image_results.append([
-            str(file_num).zfill(5),
-            tmp_img.thumbnail_url,
-            tmp_img.content_url])
-        file_num += 1
+        for tmp_img in tmp_results.value:
+            image_results.append([
+                str(file_num).zfill(5),
+                tmp_img.thumbnail_url,
+                tmp_img.content_url])
+            file_num += 1
+        sleep(1)
 
     # listをnp.arrayに変換してreturn
     return np.array(image_results)
@@ -79,15 +82,19 @@ def dlImages(img_list, save_path):
 
 
 def download_image(url, timeout=10):
-    response = requests.get(url, allow_redirects=True, timeout=timeout)
-    if response.status_code != 200:
-        error = Exception("HTTP status: " + response.status_code)
-        raise error
+    try:
+        response = requests.get(url, allow_redirects=True, timeout=timeout)
+        if response.status_code != 200:
+            # error = Exception("HTTP status: " + str(response.status_code))
+            return False
 
-    content_type = response.headers["content-type"]
-    if 'image' not in content_type:
-        error = Exception("Content-Type: " + content_type)
-        raise error
+        content_type = response.headers["content-type"]
+        if 'image' not in content_type:
+            # error = Exception("Content-Type: " + content_type)
+            return False
+    except Exception as e:
+        print(e)
+        return False
 
     return response.content
 
@@ -105,18 +112,21 @@ def get_imgs_from_urlfile(urlfile, save_dir):
             img_url = urllib.parse.urlunparse(url_parsed)
             ret_content = download_image(img_url)
             # ディレクトリ名 + 00000からの通し番号 + 拡張子.xxx
-            name_str = save_dir + \
-                str(name_num).zfill(5) + os.path.splitext(img_url)[-1]
-            save_image(name_str, ret_content)
+            if ret_content:
+                # encoded_name = os.path.splitext(img_url)[-1].encode("UTF-8")
+                encoded_name = img_url.encode("UTF-8")
+                hashed_name = hashlib.sha3_256(encoded_name).hexdigest()
+                name_str = os.path.join(save_dir, hashed_name +
+                                        os.path.splitext(img_url)[-1].lower())
+                save_image(name_str, ret_content)
             name_num += 1
 
 
 def run_scraping(dir, search_q):
-    image_results = getImageUrls(search_q, 10, 30)
+    image_results = getImageUrls(search_q, 30, 510)
     writeResults(image_results[:, 0], dir + "results_numAndurl.txt")
     writeResults(image_results[:, 2], dir + "results_contents.txt")
     get_imgs_from_urlfile(dir + "results_contents.txt", dir)
-    pass
 
 
 if __name__ == '__main__':
@@ -128,4 +138,10 @@ if __name__ == '__main__':
     +filterui:aspect-tall
     スーツ+男性+filterui:photo-photo
     '''
-    run_scraping("imgs/men_suit/", "男性+スーツ")
+    q_setting = "+filterui:photo-photo+filterui:aspect-tall"
+#    run_scraping("imgs/men_suit/",
+#                 "男性 スーツ 全身"+q_setting)
+#    run_scraping("imgs/men_casual/",
+#                 "男性 カジュアル 全身 -スーツ"+q_setting)
+    run_scraping("imgs/test/",
+                 "男性 全身"+q_setting)
